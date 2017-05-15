7daa6969e21778af0cb1cd30db93aa9f21b2b6de
Elan Ruusamäe <glen@delfi.ee>
2017-05-15 19:29:17 +0300

    setup phpunit

---
+++ tests/TestCase.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the smarty-gettext/tsmarty2c package.
+ *
+ * @copyright (c) Elan Ruusamäe
+ * @license BSD
+ * @see https://github.com/smarty-gettext/tsmarty2c
+ *
+ * For the full copyright and license information,
+ * please see the LICENSE and AUTHORS files
+ * that were distributed with this source code.
+ */
+
+namespace SmartyGettext\Test;
+
+use PHPUnit_Framework_TestCase;
+
+class TestCase extends PHPUnit_Framework_TestCase {
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * This file is part of the smarty-gettext/tsmarty2c package.
+ *
+ * @copyright (c) Elan Ruusamäe
+ * @license BSD
+ * @see https://github.com/smarty-gettext/tsmarty2c
+ *
+ * For the full copyright and license information,
+ * please see the LICENSE and AUTHORS files
+ * that were distributed with this source code.
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
