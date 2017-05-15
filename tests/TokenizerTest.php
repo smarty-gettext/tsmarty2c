09d733c10d85a5491d54c42c47257904d006e6e4
Elan Ruusamäe <glen@delfi.ee>
2017-05-15 23:18:01 +0300

    add trivial token test

---
+++ tests/TokenizerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the smarty-gettext/tsmarty2c package.
+ *
+ * @copyright (c) Elan Ruusamäe
+ * @license BSD
+ * @see https://github.com/smarty-gettext/tsmarty2c
+ *
+ * For the full copyright and license information,
+ * please see the LICENSE and AUTHORS files
+ * that were distributed with this source code.
+ */
+
+namespace SmartyGettext\Test;
+
+use Smarty;
+use SmartyGettext\Tokenizer\Tokenizer;
+
+class TokenizerTest extends TestCase {
+
+	public function test1() {
+		$tokens = $this->getTokens(__DIR__ . '/data/1.html');
+		$this->assertCount(9, $tokens);
+
+		// {t name="sagi"}
+		$this->assertEquals('t', $tokens[1]->name);
+		$this->assertEquals('"sagi"', $tokens[1]->arguments[0]['name']);
+
+		// text content
+		$this->assertEquals('my name is %1', $tokens[2]->text);
+
+		// {/t}
+		$this->assertEquals('tclose', $tokens[3]->name);
+	}
+
+	/**
+	 * @param string $template
+	 * @return array
+	 */
+	private function getTokens($template) {
+		$smarty = new Smarty();
+		$tokenizer = new Tokenizer($smarty);
+
+		return $tokenizer->getTokens($template);
+	}
+}
