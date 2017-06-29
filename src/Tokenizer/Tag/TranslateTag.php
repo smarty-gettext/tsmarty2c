f8677d59a3b8b36ec99c9a9b68bc8708b69b007e
Elan Ruusamäe <glen@delfi.ee>
2017-06-29 16:29:53 +0300

    add code to parse tokens to tag objects

---
+++ src/Tokenizer/Tag/TranslateTag.php
@@ -0,0 +1,114 @@
+<?php
+
+/*
+ * This file is part of the smarty-gettext/tsmarty2c package.
+ *
+ * @copyright (c) Elan Ruusamäe
+ * @license BSD
+ * @see https://github.com/smarty-gettext/tsmarty2c
+ *
+ * For the full copyright and license information,
+ * please see the LICENSE and AUTHORS files
+ * that were distributed with this source code.
+ */
+
+namespace SmartyGettext\Tokenizer\Tag;
+
+class TranslateTag {
+	const CONTEXT = 'context';
+
+	const PLURAL = 'plural';
+
+	/** @var string */
+	public $message;
+
+	/** @var array */
+	public $arguments;
+
+	/** @var int */
+	public $line;
+
+	/**
+	 * @param string $text
+	 * @param array $arguments
+	 * @param int $line
+	 */
+	public function __construct($text, $arguments, $line) {
+		$this->message = $text;
+		$this->line = $line;
+		$this->arguments = $this->parseArguments($arguments);
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getLine() {
+		return $this->line;
+	}
+
+	/**
+	 * @return string|null
+	 */
+	public function getMessage() {
+		if (isset($this->message)) {
+			return $this->message;
+		}
+
+		return null;
+	}
+
+	/**
+	 * @return string|null
+	 */
+	public function getContext() {
+		if (isset($this->arguments[self::CONTEXT])) {
+			return $this->arguments[self::CONTEXT];
+		}
+
+		return null;
+	}
+
+	/**
+	 * @return string|null
+	 */
+	public function getPlural() {
+		if (isset($this->arguments[self::PLURAL])) {
+			return $this->arguments[self::PLURAL];
+		}
+
+		return null;
+	}
+
+	/**
+	 * Get arguments in sane way
+	 *
+	 * @return array
+	 */
+	private function parseArguments($rawArguments) {
+		$args = array();
+		foreach ($rawArguments as $arg) {
+			foreach ($arg as $key => $value) {
+				$args[$key] = $this->unquote($value);
+			}
+		}
+
+		return $args;
+	}
+
+	private function unquote($string) {
+		if ($string[0] == '"' || $string[0] == "'") {
+			$string = substr($string, 1, -1);
+		}
+
+		return $string;
+	}
+
+	public function __toString() {
+		$args = array();
+		foreach ($this->arguments as $key => $value) {
+			$args[] = sprintf("%s=%s", $key, var_export($value, 1));
+		}
+
+		return sprintf("{t %s}%s{/t}\n", join(" ", $args), $this->message);
+	}
+}
---
+++ tests/TokenParserTest.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of the smarty-gettext/tsmarty2c package.
+ *
+ * @copyright (c) Elan Ruusamäe
+ * @license BSD
+ * @see https://github.com/smarty-gettext/tsmarty2c
+ *
+ * For the full copyright and license information,
+ * please see the LICENSE and AUTHORS files
+ * that were distributed with this source code.
+ */
+
+namespace SmartyGettext\Test;
+
+use Geekwright\Po\PoFile;
+use SmartyGettext\PoInitSmartyGettext;
+use SmartyGettext\Tokenizer\TokenParser;
+
+class TokenParserTest extends TestCase
+{
+    /** @var TokenParser */
+    private $tokenParser;
+
+    public function setUp()
+    {
+        $this->tokenParser = new TokenParser();
+    }
+
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testParse($fileName, $exp)
+    {
+        $res = $this->getTags($fileName);
+        $this->assertEquals($exp, $res);
+    }
+
+    public function dataProvider()
+    {
+        return array(
+            array('1.html', array(
+                "{t name='sagi'}my name is %1{/t}\n",
+                "{t 1='one' 2='two ' 3='three'}The 1st parameter is %1, the 2nd is %2\nand the 3nd %3.{/t}\n",
+            )),
+        );
+    }
+
+    private function getTags($fileName)
+    {
+        $templateFile = __DIR__ . '/data/' . $fileName;
+        $tags = $this->tokenParser->getTranslateTags($templateFile);
+
+        $res = array();
+        foreach ($tags as $t) {
+            $res[] = (string)$t;
+        }
+
+        return $res;
+    }
+}
