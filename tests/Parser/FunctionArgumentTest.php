cb866fef4f9a0577bc52aaeea5372f3cf7bf512c
Elan Ruusamäe <glen@pld-linux.org>
2017-11-12 04:34:14 +0200

    test for nested blocks
    smarty parses it, tsmarty2c does not

---
+++ tests/Parser/FunctionArgumentTest.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the smarty-gettext/tsmarty2c package.
+ *
+ * @copyright (c) Elan Ruusamäe
+ * @license BSD
+ * @see https://github.com/smarty-gettext/tsmarty2c
+ *
+ * For the full copyright and license information,
+ * please see the LICENSE and AUTHORS files
+ * that were distributed with this source code.
+ */
+
+namespace SmartyGettext\Test\Parser;
+
+use SmartyGettext\Test\TestCase;
+
+class FunctionArgumentTest extends TestCase
+{
+    public function testTranslationInArgument()
+    {
+        $p = $this->parseTemplate('translation_in_argument.tpl');
+        $this->assertNotNull($p);
+
+        $entries = $p->getPoFile()->getEntries();
+        $this->assertCount(1, $entries);
+    }
+}
